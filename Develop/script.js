2f36ba612b6054c4996ba94a1f5351646859b1bc
Zoinks4us <brown.jordan.charles@icloud.com>
2024-04-17 17:48:15 -0400

    used day.js documentation to assign color codes to each timeblock

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,51 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var todaysTime = $('#currentDay');
+var currentClock = dayjs().format('H');
+console.log(currentClock);
+
+
+
+
+
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+ 
+    function currentTime(){
+      const now = dayjs(); // Get the current date and time
+      const formattedDateTime = now.format('YYYY-MM-DD HH:mm:ss'); 
+      todaysTime.text(formattedDateTime);
+      console.log(formattedDateTime); 
+    }
+    currentTime();
+    setInterval(currentTime, 1000);
+    blockColor();
+    listEntry();
+  
+    function blockColor(){
+       $('.time-block').each(function(){
+        const setHour = parseInt(this.id);
+        $(this).toggleClass('past', setHour < currentClock);
+        $(this).toggleClass('present', setHour === currentClock);
+        $(this).toggleClass('future', setHour > currentClock);
+      });
+    }
+
+    function listEntry(){
+      $('.saveBtn').on('click', function(){
+        const time = $(this).parent().attr('id');
+        const activity = $(this).siblings('.description').val();
+        localStorage.setItem(time, activity);
+      })
+    }
+    $('.time-block').each(function() {
+      const time = $(this).attr('id');
+      const activity = localStorage.getItem(time);
+      $(this).children('.description').val(activity);
+    });
+    
+
+    
+
+  });
+  
+  
+
